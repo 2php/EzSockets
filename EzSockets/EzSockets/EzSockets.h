6aa25966796b90de713e408782b1af67bff9bfbe
Rémi Pelletier <remi_pelletier_6@hotmail.com>
2016-08-18 14:00:46 -0400

    Update EzSockets.h

--- EzSockets/EzSockets/EzSockets.h
+++ EzSockets/EzSockets/EzSockets.h
@@ -1,8 +1,8 @@
-//Author: Rémi Pelletier
+//Author: RÃ©mi Pelletier
 //File:   EzSockets.h
 //Desc.:  EzSockets dependencies header.
 
-//TODO: Add POSIX EzSocket implementation header.
+//TODO: Add headers and conditions for the Linux and OSX wrappers.
 
 #pragma once
 #include "EzSocket.h"
